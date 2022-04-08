b9f2100eefbdaef7c3cec204ef3127c4944fcf60
charlemagne <agbolossouc@gmail.com>
2022-04-08 09:34:11 -0700

    5 number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - print size of various types
+* Return: the 0 (Success)
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; ++digit)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}
