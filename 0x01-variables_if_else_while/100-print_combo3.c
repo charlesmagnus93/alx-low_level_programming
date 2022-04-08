20d2c729912c3d13426a05c64c4736454f929733
charlemagne <agbolossouc@gmail.com>
2022-04-08 10:57:00 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+* main - print size of various types
+* Return: the 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+	int b;
+
+	for (num = 49; num <= 57; num++)
+	{
+		for(b = 48; b < num; b++)
+		{
+			if (num != 49)
+				putchar(',');
+			putchar(b);
+			putchar(num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
