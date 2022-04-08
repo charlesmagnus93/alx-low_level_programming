0cd1e25a050f0971b0265b76dd4c084197313768
charlemagne <agbolossouc@gmail.com>
2022-04-08 09:13:52 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - print size of various types
+* Return: the 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	char a = '\n';
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar(a);
+	return (0);
+}
