5df838de18319f3228f69f7a9b4d5380bb0e1bf7
charlemagne <agbolossouc@gmail.com>
2022-04-08 09:52:01 -0700

    base26

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - print size of various types
+* Return: the 0 (Success)
+*/
+
+int main(void)
+{
+	cahr base_16;
+
+	for (base_16 = 48; base_16 <= 57; ++base_16)
+	{
+		putchar(base_16);
+	}
+	for (base_16 = 97; base_16 <= 102; ++base_16)
+	{
+		putchar(base_16);
+	}
+	return (0);
+}	
