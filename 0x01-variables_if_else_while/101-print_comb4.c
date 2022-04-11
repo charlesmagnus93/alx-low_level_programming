065168258f299244385f66caef4d39fcec4b9e7b
charlemagne <agbolossouc@gmail.com>
2022-04-11 13:03:22 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,34 +6,34 @@
 
 int main(void)
 {
-        int num;
-        int a;
-        int b;
+	int num;
+	int a;
+	int b;
 
-        for (num = 48; num <= 57; num++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        for (a = 49; a <= 57; a++)
-                        {
-                                if (num >= a || num >= b)
-                                {
-                                        continue;
-                                } else
-                                {
-                                        putchar(num);
-                                        putchar(a);
-                                        putchar(b);
-                                        if ((num == 56 && b == 57) || (num == 56 && a == 57))
-                                        {
-                                                continue;
-                                        }
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (num = 48; num <= 57; num++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			for (a = 49; a <= 57; a++)
+			{
+				if (num >= a || num >= b)
+				{
+					continue;
+				} else
+				{
+					putchar(num);
+					putchar(a);
+					putchar(b);
+					if ((num == 56 && b == 57) || (num == 56 && a == 57))
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			}
+	}
+	putchar('\n');
+	return (0);
 }
