495c587995a529f01fb7e17dc4135d8b99de7845
charlemagne <agbolossouc@gmail.com>
2022-04-13 06:09:52 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
-		return (+1);
+		_putchar("+");
+		return (1);
 	};
 	if (n < 0)
 	{
-		printf("-");
+		_putchar("-");
 		return (-1);
 	};
-	printf("0");
+	_putchar("0");
 	return (0);
 }
