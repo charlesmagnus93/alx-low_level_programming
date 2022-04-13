b8b3def7bd5651996d1db08c6e13420c31a2ce8e
charlemagne <agbolossouc@gmail.com>
2022-04-13 09:25:54 -0700

    debug-0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - test function that print if nagative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int m, int d, int y);
-int convert_day(int m, int d);
 
 #endif /* MAIN_H */
