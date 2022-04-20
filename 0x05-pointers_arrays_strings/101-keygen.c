955241444d979162cccaa2e4ef0ffb044aae31e6
charlemagne <agbolossouc@gmail.com>
2022-04-20 16:33:44 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - print password.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int ascii = 2772;
+    int i = 0;
+    int j, random;
+	char pwd[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		pwd[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		pwd[i] = ascii;
+	else
+	{
+		i--;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", pwd[j]);
+	}
+	return (0);
+}
