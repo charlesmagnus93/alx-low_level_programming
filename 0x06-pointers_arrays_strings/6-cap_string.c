0f1d175bd9026e321709b41cabb135db6bfb8eaf
charlemagne <agbolossouc@gmail.com>
2022-04-24 18:33:59 -0700

    6 cap str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,12 @@ char *cap_string(char *str)
 	int i = 1, j, check;
 	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 
-	if (str[0] > 'a' && str[0] < 'z')
+	if (str[0] > 96 && str[0] < 123)
 		str[0] -= 32;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] > 'a' && str[i] < 'z')
+		if (str[i] > 96 && str[i] < 123)
 		{
 			j = 0;
 			check = 0;
