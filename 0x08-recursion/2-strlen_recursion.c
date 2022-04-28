f1372244964bb875f5d2479bdeeaf82779f46505
charlemagne <agbolossouc@gmail.com>
2022-04-28 10:39:40 -0700

    2 strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of string
+ * @s: string
+ *
+ * Return: in as string length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
