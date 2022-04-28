65a563bd392157aee49f744d89e9ad45d6e97aa3
charlemagne <agbolossouc@gmail.com>
2022-04-28 11:31:43 -0700

    6- is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include <math.h>
+
+/**
+ * prime - evaluate
+ * @a: same number as n
+ * @b: iterate
+ *
+ * Return: integer
+ */
+int prime(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime(a, b + 1));
+}
+
+/**
+ * is_prime_number - prime number
+ * @n: integer
+ *
+ * Return: in as square val
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
