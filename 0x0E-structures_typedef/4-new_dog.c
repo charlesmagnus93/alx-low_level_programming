86a78fcd0472f7c94e32299277e648b3bee81c9c
charlemagne <agbolossouc@gmail.com>
2022-05-09 11:13:22 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _copy - make copy.
+ * @str: string to copy
+ *
+ * Return: Pointer
+ */
+char *_copy(char *str)
+{
+	int l, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	l = 0;
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+	{
+		*(s + i) = *(str + i);
+	}
+
+	return (s);
+}
+
+/**
+ * *new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: 1
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == 0 || name == 0 || owner == 0)
+		return (0);
+
+	new_dog->name = _copy(name);
+	if (new_dog->name == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
+	new_dog->age = age;
+	new_dog->owner = _copy(owner);
+	if (new_dog->owner == 0)
+	{
+		free(new_dog);
+		return (0);
+	}
+	return (new_dog);
+}
