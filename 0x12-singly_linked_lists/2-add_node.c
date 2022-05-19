b13cef48f089d4a9535711e2cf87c80c99cc93f5
charlemagne <agbolossouc@gmail.com>
2022-05-19 14:21:46 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * *add_node - add new node.
+ * @head: head linked list.
+ * @str: string.
+ * Return: address.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t nextChar;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (nextChar = 0; str[nextChar]; nextChar++);
+    
+	new->len = nextChar;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
