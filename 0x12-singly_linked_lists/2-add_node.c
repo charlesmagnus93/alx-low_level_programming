1072f89284c93f6d3bdd1e363979984e55cc41b3
charlemagne <agbolossouc@gmail.com>
2022-05-19 14:24:19 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nextChar = 0; str[nextChar]; nextChar++);
+	for (nextChar = 0; str[nextChar]; nextChar++)
+		;
 
 	new->len = nextChar;
 	new->next = *head;
