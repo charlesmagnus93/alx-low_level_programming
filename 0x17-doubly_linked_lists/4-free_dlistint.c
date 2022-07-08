3b19686c490f87bc9d445db95f0d7fe6dc3d359f
charlemagne <agbolossouc@gmail.com>
2022-07-08 10:54:48 -0700

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free
+ * @head: head linked list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	int ln = 0;
+	int i;
+
+	while (head != NULL)
+	{
+		ln++;
+		head = head->next;
+	}
+
+	for (i = 0; i < ln; i++)
+	{
+		free(head);
+	}
+}
