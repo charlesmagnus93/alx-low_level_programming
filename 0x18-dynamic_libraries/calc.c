aa4705b29c482934ba1dd32da4b3718c3057618a
charlemagne <agbolossouc@gmail.com>
2022-07-11 11:11:08 -0700

    ctypes

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - calculates the sum
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - calculates the difference
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - calculates the product
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - calculates the division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculates modulo
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: modulo val
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
